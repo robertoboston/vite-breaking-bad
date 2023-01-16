345f4d2696d2dee23ceb39f527b73161e1ff9e69
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-16 17:59:00 +0100

    store.js imported

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,25 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
 
+
+
+export default {
+    data(){
+        return{
+            store
+        }
+    },
+    
+}
+
+
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
