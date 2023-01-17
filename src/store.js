10b63177a97ab8f8d5cc84527840097dbe522021
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-17 14:00:00 +0100

    start from zero

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { reactive } from 'vue'
-
-export const store = reactive({
-    ulr: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien',
-    cardList : [],
-    loading : true
-
-})
